df2a35224647cc88fa4afbfc49bd4d5090a46fc7
Johan Nilsson <github@markupartist.com>
2009-10-29 22:16:49 +0100

    added search history, replaced handler with SearchRoutesTask

--- src/com/markupartist/sthlmtraveling/SearchActivity.java
+++ src/com/markupartist/sthlmtraveling/SearchActivity.java
@@ -2,25 +2,26 @@ package com.markupartist.sthlmtraveling;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.Menu;
@@ -32,24 +33,29 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class SearchActivity extends Activity {
+import com.markupartist.sthlmtraveling.SearchRoutesTask.OnSearchRoutesResultListener;
+import com.markupartist.sthlmtraveling.provider.HistoryDbAdapter;
+
+public class SearchActivity extends Activity implements OnSearchRoutesResultListener {
     private static final String TAG = "Search";
     private static final int DIALOG_START_POINT = 0;
     private static final int DIALOG_END_POINT = 1;
-    private static final int DIALOG_NO_ROUTES_FOUND = 2;
-    private static final int DIALOG_ABOUT = 3;
-    private static final int DIALOG_PROGRESS = 4;
+    private static final int DIALOG_ABOUT = 2;
+    private static final int DIALOG_START_POINT_HISTORY = 3;
+    private static final int DIALOG_END_POINT_HISTORY = 4;
 
     private AutoCompleteTextView mFromAutoComplete;
     private AutoCompleteTextView mToAutoComplete;
-
-    private final Handler mHandler = new Handler();
+    private HistoryDbAdapter mHistoryDbAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
 
+        mHistoryDbAdapter = new HistoryDbAdapter(this).open();
+
         Planner planner = Planner.getInstance();
+
         mFromAutoComplete = (AutoCompleteTextView) findViewById(R.id.from);
         AutoCompleteStopAdapter stopAdapter = new AutoCompleteStopAdapter(this, 
                 android.R.layout.simple_dropdown_item_1line, planner);
@@ -91,54 +97,15 @@ public class SearchActivity extends Activity {
             } else {
                 Time time = new Time();
                 time.setToNow();
-                searchRoutes(mFromAutoComplete.getText().toString(), 
+
+                SearchRoutesTask searchRoutesTask = 
+                    new SearchRoutesTask(SearchActivity.this)
+                        .setOnSearchRoutesResultListener(SearchActivity.this);
+                searchRoutesTask.execute(mFromAutoComplete.getText().toString(), 
                         mToAutoComplete.getText().toString(), time);
             }
         }
     };
-
-    /**
-     * Fires off a thread to do the query. Will call onSearchResult when done.
-     * @param startPoint the start point
-     * @param endPoint the end point
-     */
-    private void searchRoutes(final String startPoint, final String endPoint, final Time time) {
-        showDialog(DIALOG_PROGRESS);
-        new Thread() {
-            public void run() {
-                try {
-                    Planner.getInstance().findRoutes(startPoint, endPoint, time);
-                    mHandler.post(new Runnable() {
-                        @Override public void run() {
-                            onSearchRoutesResult();
-                        }
-                    });
-                    dismissDialog(DIALOG_PROGRESS);
-                } catch (Exception e) {
-                    dismissDialog(DIALOG_PROGRESS);
-                }
-            }
-        }.start();
-    }
-
-    /**
-     * Called when we have a search result for routes.
-     */
-    private void onSearchRoutesResult() {
-        if (Planner.getInstance().lastFoundRoutes() != null 
-                && !Planner.getInstance().lastFoundRoutes().isEmpty()) {
-            Intent i = new Intent(SearchActivity.this, RoutesActivity.class);
-            i.putExtra("com.markupartist.sthlmtraveling.startPoint", 
-                    mFromAutoComplete.getText().toString());
-            i.putExtra("com.markupartist.sthlmtraveling.endPoint", 
-                    mToAutoComplete.getText().toString());
-            startActivity(i);
-        } else {
-            // TODO: This works for now, but we need to see if there are any
-            // alternative stops available in later on.
-            showDialog(DIALOG_NO_ROUTES_FOUND);
-        }
-    }
 
     @Override
     protected Dialog onCreateDialog(int id) {
@@ -147,9 +114,16 @@ public class SearchActivity extends Activity {
         case DIALOG_START_POINT:
             AlertDialog.Builder startPointDialogBuilder = new AlertDialog.Builder(this);
             startPointDialogBuilder.setTitle("Choose start point");
-            startPointDialogBuilder.setItems(getMyLocationItems(), new DialogInterface.OnClickListener() {
+            startPointDialogBuilder.setItems(getDialogSelectPointItems(), new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int item) {
-                    mFromAutoComplete.setText(getAddressFromCurrentPosition());
+                    switch(item) {
+                    case 0:
+                        mFromAutoComplete.setText(getAddressFromCurrentPosition());
+                        break;
+                    case 1:
+                        showDialog(DIALOG_START_POINT_HISTORY);
+                        break;
+                    }
                 }
             });
             dialog = startPointDialogBuilder.create();
@@ -157,23 +131,20 @@ public class SearchActivity extends Activity {
         case DIALOG_END_POINT:
             AlertDialog.Builder endPointDialogBuilder = new AlertDialog.Builder(this);
             endPointDialogBuilder.setTitle("Choose end point");
-            endPointDialogBuilder.setItems(getMyLocationItems(), new DialogInterface.OnClickListener() {
+            endPointDialogBuilder.setItems(getDialogSelectPointItems(), 
+                    new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int item) {
-                    mToAutoComplete.setText(getAddressFromCurrentPosition());
+                    switch(item) {
+                    case 0:
+                        mToAutoComplete.setText(getAddressFromCurrentPosition());
+                        break;
+                    case 1:
+                        showDialog(DIALOG_END_POINT_HISTORY);
+                        break;
+                    }
                 }
             });
             dialog = endPointDialogBuilder.create();
-            break;
-        case DIALOG_NO_ROUTES_FOUND:
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            dialog = builder.setTitle("Unfortunately no routes was found")
-                .setMessage("If searhing for an address try adding a house number.")
-                .setCancelable(true)
-                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                   }
-                }).create();
             break;
         case DIALOG_ABOUT:
             PackageManager pm = getPackageManager();
@@ -185,25 +156,71 @@ public class SearchActivity extends Activity {
                 Log.e(TAG, "Could not get the package info.");
             }
 
-            dialog = new Dialog(this);
-            dialog.setCanceledOnTouchOutside(true);
-            dialog.setContentView(R.layout.about_dialog);
-            dialog.setTitle(getText(R.string.app_name) + " " + version);
-            break;
-        case DIALOG_PROGRESS:
-            ProgressDialog progress = new ProgressDialog(this);
-            progress.setMessage(getText(R.string.loading));
-            dialog = progress;
-            break;
+            return new AlertDialog.Builder(this)
+                .setTitle(getText(R.string.app_name) + " " + version)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setMessage(getText(R.string.about_this_app))
+                .setCancelable(true)
+                .setPositiveButton(getText(android.R.string.ok), null)
+                .setNeutralButton(getText(R.string.donate), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://pledgie.com/campaigns/6527"));
+                        startActivity(browserIntent);
+                    }
+                })
+                .setNegativeButton(getText(R.string.feedback), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        emailIntent .setType("plain/text");
+                        emailIntent .putExtra(android.content.Intent.EXTRA_EMAIL,
+                                new String[]{"sthlmtraveling@markupartist.com"});
+                        emailIntent .putExtra(android.content.Intent.EXTRA_SUBJECT,
+                                "iglaset feedback");
+                        startActivity(Intent.createChooser(emailIntent,
+                                getText(R.string.send_email)));
+                    }
+                })
+                .create();
+        case DIALOG_START_POINT_HISTORY:
+            final Cursor startPointCursor = mHistoryDbAdapter.fetchAllStartPoints();
+            startManagingCursor(startPointCursor);
+            Log.d(TAG, "startPoints: " + startPointCursor.getCount());
+            return new AlertDialog.Builder(this)
+                .setTitle("History")
+                .setCursor(startPointCursor, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int index = startPointCursor.getColumnIndex(HistoryDbAdapter.KEY_NAME);
+                        mFromAutoComplete.setText(startPointCursor.getString(index));
+                    }
+                }, HistoryDbAdapter.KEY_NAME)
+                .create();
+        case DIALOG_END_POINT_HISTORY:
+            final Cursor endPointCursor = mHistoryDbAdapter.fetchAllEndPoints();
+            startManagingCursor(endPointCursor);
+            Log.d(TAG, "endPoints: " + endPointCursor.getCount());
+            return new AlertDialog.Builder(this)
+                .setTitle("History")
+                .setCursor(endPointCursor, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int index = endPointCursor.getColumnIndex(HistoryDbAdapter.KEY_NAME);
+                        mToAutoComplete.setText(endPointCursor.getString(index));
+                    }
+                }, HistoryDbAdapter.KEY_NAME)
+                .create();
         }
         return dialog;
     }
-    
-    private CharSequence[] getMyLocationItems() {
-        CharSequence[] items = {"My Location"};
+
+    private CharSequence[] getDialogSelectPointItems() {
+        CharSequence[] items = {"My Location", "History"};
         return items;
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -271,7 +288,33 @@ public class SearchActivity extends Activity {
             case R.id.about:
                 showDialog(DIALOG_ABOUT);
                 return true;
+            case R.id.reverse_start_end:
+                String startPoint = mFromAutoComplete.getText().toString();
+                String endPoint = mToAutoComplete.getText().toString();
+                mFromAutoComplete.setText(endPoint);
+                mToAutoComplete.setText(startPoint);
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHistoryDbAdapter.close();
+    }
+
+    @Override
+    public void onSearchRoutesResult(ArrayList<Route> routes) {
+        String startPoint = mFromAutoComplete.getText().toString();
+        String endPoint = mToAutoComplete.getText().toString();
+
+        mHistoryDbAdapter.create(HistoryDbAdapter.TYPE_START_POINT, startPoint);
+        mHistoryDbAdapter.create(HistoryDbAdapter.TYPE_END_POINT, endPoint);
+
+        Intent i = new Intent(SearchActivity.this, RoutesActivity.class);
+        i.putExtra("com.markupartist.sthlmtraveling.startPoint", startPoint);
+        i.putExtra("com.markupartist.sthlmtraveling.endPoint", endPoint);
+        startActivity(i);
     }
 }
