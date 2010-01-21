55d738fdd23c2f590696e29a2256c6e23adacdf0
Johan Nilsson <github@markupartist.com>
2010-01-21 20:58:58 +0100

    workaround for the MyLocationOverlay that causes crash on some devices

--- src/com/markupartist/sthlmtraveling/PointOnMapActivity.java
+++ src/com/markupartist/sthlmtraveling/PointOnMapActivity.java
@@ -3,7 +3,13 @@ package com.markupartist.sthlmtraveling;
 import java.io.IOException;
 import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Paint.Style;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -21,6 +27,7 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Projection;
 import com.markupartist.sthlmtraveling.graphics.LabelMarker;
 import com.markupartist.sthlmtraveling.provider.planner.Stop;
 
@@ -166,7 +173,8 @@ public class PointOnMapActivity extends MapActivity {
             (LocationManager)getSystemService(LOCATION_SERVICE);
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            mMyLocationOverlay = new MyLocationOverlay(this, mMapView);
+            //mMyLocationOverlay = new MyLocationOverlay(this, mMapView);
+            mMyLocationOverlay = new FixedMyLocationOverlay(this, mMapView);
             mMapView.getOverlays().add(mMyLocationOverlay);
             mMyLocationOverlay.enableCompass();
             mMyLocationOverlay.enableMyLocation();
@@ -260,5 +268,74 @@ public class PointOnMapActivity extends MapActivity {
             Log.d(TAG, "Failed to get name " + e.getMessage());
         }
         return name;
+    }
+
+    /**
+     * From the Android Developers group.
+     * http://groups.google.com/group/android-developers/msg/6cb65da690614662
+     * 
+     * This class is a workaround for the MyLocationOverlay, that crashes on
+     * some devices, in my case I got reports from a Spica device.
+     */
+    public class FixedMyLocationOverlay extends MyLocationOverlay {
+        private boolean bugged = false;
+        private Paint accuracyPaint;
+        private Point center;
+        private Point left;
+        private Drawable drawable;
+        private int width;
+        private int height;
+
+        public FixedMyLocationOverlay(Context context, MapView mapView) {
+            super(context, mapView);
+        }
+
+        @Override
+        protected void drawMyLocation(Canvas canvas, MapView mapView,
+                Location lastFix, GeoPoint myLoc, long when) {
+            if (!bugged) {
+                try {
+                    super.drawMyLocation(canvas, mapView, lastFix, myLoc, when);
+                } catch (Exception e) {
+                    bugged = true;
+                }
+            }
+            if (bugged) {
+                if (drawable == null) {
+                    accuracyPaint = new Paint();
+                    accuracyPaint.setAntiAlias(true);
+                    accuracyPaint.setStrokeWidth(2.0f);
+                    drawable = mapView.getContext().getResources().getDrawable(
+                            R.drawable.my_location_bugged);
+                    width = drawable.getIntrinsicWidth();
+                    height = drawable.getIntrinsicHeight();
+                    center = new Point();
+                    left = new Point();
+                }
+                Projection projection = mapView.getProjection();
+                double latitude = lastFix.getLatitude();
+                double longitude = lastFix.getLongitude();
+                float accuracy = lastFix.getAccuracy();
+                float[] result = new float[1];
+                Location.distanceBetween(latitude, longitude, latitude,
+                        longitude + 1, result);
+                float longitudeLineDistance = result[0];
+                GeoPoint leftGeo = new GeoPoint(
+                        (int) (latitude * 1e6),
+                        (int) ((longitude - accuracy / longitudeLineDistance) * 1e6));
+                projection.toPixels(leftGeo, left);
+                projection.toPixels(myLoc, center);
+                int radius = center.x - left.x;
+                accuracyPaint.setColor(0xff6666ff);
+                accuracyPaint.setStyle(Style.STROKE);
+                canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
+                accuracyPaint.setColor(0x186666ff);
+                accuracyPaint.setStyle(Style.FILL);
+                canvas.drawCircle(center.x, center.y, radius, accuracyPaint);
+                drawable.setBounds(center.x - width / 2, center.y - height / 2,
+                        center.x + width / 2, center.y + height / 2);
+                drawable.draw(canvas);
+            }
+        }
     }
 }
