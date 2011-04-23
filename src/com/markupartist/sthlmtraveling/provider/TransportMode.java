7e4cabaf871ad14427171aa93ac1b59220d6f7e5
Johan Nilsson <github@markupartist.com>
2011-04-23 10:02:58 +0200

    Changed order.

--- src/com/markupartist/sthlmtraveling/provider/TransportMode.java
+++ src/com/markupartist/sthlmtraveling/provider/TransportMode.java
@@ -7,13 +7,13 @@ public class TransportMode {
     public final static int TRAIN_INDEX = 2;
     public final static int LOKALBANA_INDEX = 3;
 
+    public static final String UNKNOWN = "";
     public static final String BUS = "BUS";
     public static final String METRO = "MET";
     public static final String TRAIN = "TRN";
     public static final String TRAM = "TRM";
     public static final String FLY = "FLY";
     public static final String NAR = "NAR";
-    public static final String UNKNOWN = "";
     public static final String WAX = "WAX";
 
     
