a4c2c5bed99887b9b8296c27784632ef2a845869
Robert Johansson <robert.b.johansson@gmail.com>
2011-11-11 23:31:54 +0100

    Made shortcuts work again.

---
+++ src/com/markupartist/sthlmtraveling/PlannerFragmentActivity.java
@@ -0,0 +1,16 @@
+package com.markupartist.sthlmtraveling;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class PlannerFragmentActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.planner_list);
+	}
+	
+}
