4c4975deb77413ff71e812df113bbc5f68721144
Johan Nilsson <github@markupartist.com>
2011-11-23 18:43:04 +0100

    Fixed the url for getting sites.

--- src/com/markupartist/sthlmtraveling/provider/site/SitesStore.java
+++ src/com/markupartist/sthlmtraveling/provider/site/SitesStore.java
@@ -40,7 +40,7 @@ public class SitesStore {
     }
 
     public ArrayList<Site> getSiteV2(String name) throws IOException {
-        final HttpGet get = new HttpGet(apiEndpoint2() + "/site/v1/"
+        final HttpGet get = new HttpGet(apiEndpoint2() + "site/v1/"
                 + "?q=" + URLEncoder.encode(name));
         get.addHeader("X-STHLMTraveling-API-Key", get(KEY));
         HttpEntity entity = null;
