33e96584706c530333928a8d2fb0379282f62e4b
Johan Nilsson <github@markupartist.com>
2011-12-29 16:20:49 +0100

    Fix issue with auto complete on ICS.

---
+++ src/com/markupartist/sthlmtraveling/BaseFragmentActivity.java
@@ -0,0 +1,62 @@
+package com.markupartist.sthlmtraveling;
+
+import java.util.Map;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+
+import com.flurry.android.FlurryAgent;
+import com.markupartist.android.widget.ActionBar;
+import com.markupartist.android.widget.actionbar.R;
+
+public class BaseFragmentActivity extends FragmentActivity {
+
+    @Override
+    protected void onStart() {
+       super.onStart();
+       FlurryAgent.onStartSession(this, MyApplication.ANALYTICS_KEY);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FlurryAgent.onPageView();
+    }
+
+    @Override
+    protected void onStop() {
+       super.onStop();
+       FlurryAgent.onEndSession(this);
+    }
+
+    protected ActionBar initActionBar(int menuResource) {
+        ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowHomeEnabled(true);
+        getMenuInflater().inflate(menuResource, actionBar.asMenu());
+        return actionBar;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.actionbar_item_home:
+            final Intent startIntent = new Intent(this, StartActivity.class);
+            startIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(startIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void registerEvent(String event) {
+        FlurryAgent.onEvent(event);
+    }
+
+    protected void registerEvent(String event, Map<String, String> parameters) {
+        FlurryAgent.onEvent(event, parameters);
+    }
+
+}
