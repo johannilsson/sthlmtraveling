6aa8230fa5098b0c7342eeaf14970bf7023af84a
Johan Nilsson <github@markupartist.com>
2011-12-30 11:01:50 +0100

    Cosmetic fix, tabs to spaces.

--- src/com/markupartist/sthlmtraveling/SearchDeparturesFragment.java
+++ src/com/markupartist/sthlmtraveling/SearchDeparturesFragment.java
@@ -39,62 +39,64 @@ import com.markupartist.sthlmtraveling.provider.planner.Planner;
 import com.markupartist.sthlmtraveling.provider.planner.Stop;
 
 public class SearchDeparturesFragment extends BaseListFragment {
-	static String TAG = "SearchDeparturesActivity";
-	private static final int DIALOG_HISTORY = 0;
-	private static final int DIALOG_MORE = 1;
-	private boolean mCreateShortcut;
-	private HistoryDbAdapter mHistoryDbAdapter;
-	private AutoCompleteTextView mSiteTextView;
+    static String TAG = "SearchDeparturesActivity";
+    private static final int DIALOG_HISTORY = 0;
+    private static final int DIALOG_MORE = 1;
+    private boolean mCreateShortcut;
+    private HistoryDbAdapter mHistoryDbAdapter;
+    private AutoCompleteTextView mSiteTextView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		// If the activity was started with the "create shortcut" action, we
-		// remember this to change the behavior upon a search.
-		if (Intent.ACTION_CREATE_SHORTCUT.equals(getActivity().getIntent()
-				.getAction())) {
-			mCreateShortcut = true;
-			registerEvent("Create departure shortcut");
-		} else {
-			registerEvent("Search departures");
-		}
-		mHistoryDbAdapter = new HistoryDbAdapter(getActivity()).open();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.search_departures_fragment, container, false);
-	}
+        // If the activity was started with the "create shortcut" action, we
+        // remember this to change the behavior upon a search.
+        if (Intent.ACTION_CREATE_SHORTCUT.equals(getActivity().getIntent()
+                .getAction())) {
+            mCreateShortcut = true;
+            registerEvent("Create departure shortcut");
+        } else {
+            registerEvent("Search departures");
+        }
+        mHistoryDbAdapter = new HistoryDbAdapter(getActivity()).open();
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		findViews();
-		fillData();
-		super.onActivityCreated(savedInstanceState);
-	}
-	
-	private void findViews() {
-		View searchHeader = getActivity().getLayoutInflater().inflate(
-				R.layout.search_departures_header, null);
-		getListView().addHeaderView(searchHeader, null, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_departures_fragment, container,
+                false);
+    }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        findViews();
+        fillData();
+        super.onActivityCreated(savedInstanceState);
+    }
 
-		mSiteTextView = (AutoCompleteTextView) searchHeader
-				.findViewById(R.id.sites);
-		AutoCompleteStopAdapter stopAdapter = new AutoCompleteStopAdapter(
-				getActivity(), R.layout.simple_dropdown_item_1line,
-				Planner.getInstance(), false);
+    private void findViews() {
+        View searchHeader = getActivity().getLayoutInflater().inflate(
+                R.layout.search_departures_header, null);
+        getListView().addHeaderView(searchHeader, null, false);
 
-		mSiteTextView.setSelectAllOnFocus(true);
-		mSiteTextView.setAdapter(stopAdapter);
+        mSiteTextView = (AutoCompleteTextView) searchHeader
+                .findViewById(R.id.sites);
+        AutoCompleteStopAdapter stopAdapter = new AutoCompleteStopAdapter(
+                getActivity(), R.layout.simple_dropdown_item_1line,
+                Planner.getInstance(), false);
 
-		mSiteTextView.setOnEditorActionListener(new OnEditorActionListener() {
+        mSiteTextView.setSelectAllOnFocus(true);
+        mSiteTextView.setAdapter(stopAdapter);
+
+        mSiteTextView.setOnEditorActionListener(new OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            public boolean onEditorAction(TextView v, int actionId,
+                    KeyEvent event) {
                 boolean isEnterKey = (null != event && event.getKeyCode() == KeyEvent.KEYCODE_ENTER);
-                if(actionId == EditorInfo.IME_ACTION_SEARCH || true == isEnterKey) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || true == isEnterKey) {
                     dispatchSearch();
                     return true;
                 }
@@ -102,68 +104,68 @@ public class SearchDeparturesFragment extends BaseListFragment {
             }
         });
 
-		ImageButton searchButton = (ImageButton) searchHeader
-				.findViewById(R.id.search_departure);
-		if (mCreateShortcut) {
-			// searchButton.setText(getText(R.string.create_shortcut_label));
-			getActivity()
-					.setTitle(R.string.create_shortcut_for_departure_label);
-		}
-		searchButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dispatchSearch();
-			}
-		});
+        ImageButton searchButton = (ImageButton) searchHeader
+                .findViewById(R.id.search_departure);
+        if (mCreateShortcut) {
+            // searchButton.setText(getText(R.string.create_shortcut_label));
+            getActivity()
+                    .setTitle(R.string.create_shortcut_for_departure_label);
+        }
+        searchButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchSearch();
+            }
+        });
 
-	}
-	
-	@Override
-	public void onDestroyView() {
-		setListAdapter(null);
-		super.onDestroyView();
-	}
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		mHistoryDbAdapter.close();
-	}
+    @Override
+    public void onDestroyView() {
+        setListAdapter(null);
+        super.onDestroyView();
+    }
 
-	private void fillData() {
-		final Cursor historyCursor = mHistoryDbAdapter
-				.fetchByType(HistoryDbAdapter.TYPE_DEPARTURE_SITE);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mHistoryDbAdapter.close();
+    }
 
-		getActivity().startManagingCursor(historyCursor);
+    private void fillData() {
+        final Cursor historyCursor = mHistoryDbAdapter
+                .fetchByType(HistoryDbAdapter.TYPE_DEPARTURE_SITE);
 
-		String[] from = new String[] { HistoryDbAdapter.KEY_NAME };
+        getActivity().startManagingCursor(historyCursor);
 
-		int[] to = new int[] { android.R.id.text1 };
+        String[] from = new String[] { HistoryDbAdapter.KEY_NAME };
 
-		final SimpleCursorAdapter favorites = new SimpleCursorAdapter(
-				getActivity(), android.R.layout.simple_list_item_1,
-				historyCursor, from, to);
+        int[] to = new int[] { android.R.id.text1 };
 
-		getActivity().stopManagingCursor(historyCursor);
+        final SimpleCursorAdapter favorites = new SimpleCursorAdapter(
+                getActivity(), android.R.layout.simple_list_item_1,
+                historyCursor, from, to);
 
-		setListAdapter(favorites);
-	}
+        getActivity().stopManagingCursor(historyCursor);
 
-	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		Cursor historyCursor = ((SimpleCursorAdapter) this.getListAdapter())
-				.getCursor();
-		getActivity().startManagingCursor(historyCursor);
-		int index = historyCursor.getColumnIndex(HistoryDbAdapter.KEY_NAME);
-		String siteName = historyCursor.getString(index);
-		getActivity().stopManagingCursor(historyCursor);
+        setListAdapter(favorites);
+    }
 
-		if (mCreateShortcut) {
-			onCreateShortCut(siteName);
-		} else {
-			onSearchDepartures(siteName);
-		}
-	}
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Cursor historyCursor = ((SimpleCursorAdapter) this.getListAdapter())
+                .getCursor();
+        getActivity().startManagingCursor(historyCursor);
+        int index = historyCursor.getColumnIndex(HistoryDbAdapter.KEY_NAME);
+        String siteName = historyCursor.getString(index);
+        getActivity().stopManagingCursor(historyCursor);
+
+        if (mCreateShortcut) {
+            onCreateShortCut(siteName);
+        } else {
+            onSearchDepartures(siteName);
+        }
+    }
 
     private void dispatchSearch() {
         if (mSiteTextView.getText().length() == 0) {
@@ -176,36 +178,36 @@ public class SearchDeparturesFragment extends BaseListFragment {
             }
         }
     }
-    
-	private void onSearchDepartures(String siteName) {
-		Stop stop = new Stop();
-		stop.setName(siteName);
-		mHistoryDbAdapter.create(HistoryDbAdapter.TYPE_DEPARTURE_SITE, stop);
 
-		Intent i = new Intent(getActivity().getApplicationContext(),
-				DeparturesActivity.class);
-		i.putExtra(DeparturesActivity.EXTRA_SITE_NAME, siteName);
-		startActivity(i);
-	}
+    private void onSearchDepartures(String siteName) {
+        Stop stop = new Stop();
+        stop.setName(siteName);
+        mHistoryDbAdapter.create(HistoryDbAdapter.TYPE_DEPARTURE_SITE, stop);
 
-	private void onCreateShortCut(String siteName) {
-		// Setup the intent to be launched
-		Intent shortcutIntent = new Intent(getActivity()
-				.getApplicationContext(), DeparturesActivity.class);
-		shortcutIntent.setAction(Intent.ACTION_VIEW);
-		shortcutIntent.putExtra(DeparturesActivity.EXTRA_SITE_NAME, siteName);
-		shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent i = new Intent(getActivity().getApplicationContext(),
+                DeparturesActivity.class);
+        i.putExtra(DeparturesActivity.EXTRA_SITE_NAME, siteName);
+        startActivity(i);
+    }
 
-		// Then, set up the container intent (the response to the caller)
-		Intent intent = new Intent();
-		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, siteName);
-		Parcelable iconResource = Intent.ShortcutIconResource.fromContext(
-				getActivity(), R.drawable.shortcut_departures);
-		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+    private void onCreateShortCut(String siteName) {
+        // Setup the intent to be launched
+        Intent shortcutIntent = new Intent(getActivity()
+                .getApplicationContext(), DeparturesActivity.class);
+        shortcutIntent.setAction(Intent.ACTION_VIEW);
+        shortcutIntent.putExtra(DeparturesActivity.EXTRA_SITE_NAME, siteName);
+        shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-		// Now, return the result to the launcher
-		getActivity().setResult(Activity.RESULT_OK, intent);
-		getActivity().finish();
-	}
+        // Then, set up the container intent (the response to the caller)
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, siteName);
+        Parcelable iconResource = Intent.ShortcutIconResource.fromContext(
+                getActivity(), R.drawable.shortcut_departures);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+
+        // Now, return the result to the launcher
+        getActivity().setResult(Activity.RESULT_OK, intent);
+        getActivity().finish();
+    }
 }
