b2aec0109fb9cc7b0e060a4d5ab85a0b02f23e35
Johan Nilsson <github@markupartist.com>
2012-01-03 20:52:13 +0100

    Replace tabs with ViewPageIndicator.

--- src/com/markupartist/sthlmtraveling/StartActivity.java
+++ src/com/markupartist/sthlmtraveling/StartActivity.java
@@ -18,27 +18,23 @@ package com.markupartist.sthlmtraveling;
 
 import java.util.ArrayList;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TabHost;
-import android.widget.TabWidget;
-import android.widget.TextView;
 
 import com.markupartist.android.widget.ActionBar;
 import com.markupartist.sthlmtraveling.service.DeviationService;
 import com.markupartist.sthlmtraveling.utils.ErrorReporter;
+import com.viewpagerindicator.TabPageIndicator;
+import com.viewpagerindicator.TitleProvider;
 
 public class StartActivity extends BaseFragmentActivity {
-    private TabHost mTabHost;
-	private ViewPager mViewPager;
-	private TabsAdapter mTabsAdapter;
+    private PageFragmentAdapter mPageAdapter;
+    private ViewPager mPager;
+    private TabPageIndicator mIndicator;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,172 +52,97 @@ public class StartActivity extends BaseFragmentActivity {
         final ErrorReporter reporter = ErrorReporter.getInstance();
         reporter.checkErrorAndReport(this);
 
-        mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup();
-        
-        mViewPager = (ViewPager)findViewById(R.id.pager);
+        mPageAdapter = new PageFragmentAdapter(this, getSupportFragmentManager());
 
-        mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
-        mTabsAdapter.addTab(mTabHost.newTabSpec("search")
-                .setIndicator(buildIndicator(R.string.search_label)),
-                    PlannerFragment.class, null);
-        mTabsAdapter.addTab(mTabHost.newTabSpec("favorites")
-                .setIndicator(buildIndicator(R.string.favorites_label)),
-                    FavoritesFragment.class, null);
-        mTabsAdapter.addTab(mTabHost.newTabSpec("departures")
-                .setIndicator(buildIndicator(R.string.departures)),
-                    SearchDeparturesFragment.class, null);
-        mTabsAdapter.addTab(mTabHost.newTabSpec("deviations")
-                .setIndicator(buildIndicator(R.string.deviations_label)),
-                    TrafficStatusFragment.class, null);
+        mPageAdapter.addPage(new PageInfo(
+                getString(R.string.search_label), PlannerFragment.class, null));
+        mPageAdapter.addPage(new PageInfo(
+                getString(R.string.favorites_label), FavoritesFragment.class, null));
+        mPageAdapter.addPage(new PageInfo(
+                getString(R.string.departures), SearchDeparturesFragment.class, null));
+        mPageAdapter.addPage(new PageInfo(
+                getString(R.string.deviations_label), TrafficStatusFragment.class, null));
 
-        if (savedInstanceState != null) {
-            mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
-        }
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mPageAdapter);
+
+        mIndicator = (TabPageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
 
         // Start background service.
         DeviationService.startAsRepeating(getApplicationContext());
     }
 
-    /**
-     * Build a {@link View} to be used as a tab indicator, setting the requested
-     * string resource as its label.
-     * 
-     * @param textRes
-     * @return View
-     */
-    private View buildIndicator(int textRes) {
-        final TextView indicator = (TextView) getLayoutInflater()
-                .inflate(R.layout.tab_indicator,
-                        (ViewGroup) findViewById(android.R.id.tabs), false);
-        indicator.setText(getString(textRes).toUpperCase());
-        return indicator;
-    }
-    
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("tab", mTabHost.getCurrentTabTag());
-    }
+    public class PageFragmentAdapter extends FragmentPagerAdapter implements TitleProvider {
 
-    /**
-     * This is a helper class that implements the management of tabs and all
-     * details of connecting a ViewPager with associated TabHost.  It relies on a
-     * trick.  Normally a tab host has a simple API for supplying a View or
-     * Intent that each tab will show.  This is not sufficient for switching
-     * between pages.  So instead we make the content part of the tab host
-     * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
-     * view to show as the tab content.  It listens to changes in tabs, and takes
-     * care of switch to the correct paged in the ViewPager whenever the selected
-     * tab changes.
-     */
-    public static class TabsAdapter extends FragmentPagerAdapter
-            implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
-        private final Context mContext;
-        private final TabHost mTabHost;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+        private ArrayList<PageInfo> mPages = new ArrayList<PageInfo>();
+        private FragmentActivity mContext;
 
-        static final class TabInfo {
-            private final String tag;
-            private final Class<?> clss;
-            private final Bundle args;
-
-            TabInfo(String _tag, Class<?> _class, Bundle _args) {
-                tag = _tag;
-                clss = _class;
-                args = _args;
-            }
-        }
-
-        static class DummyTabFactory implements TabHost.TabContentFactory {
-            private final Context mContext;
-
-            public DummyTabFactory(Context context) {
-                mContext = context;
-            }
-
-            @Override
-            public View createTabContent(String tag) {
-                View v = new View(mContext);
-                v.setMinimumWidth(0);
-                v.setMinimumHeight(0);
-                return v;
-            }
-        }
-
-        public TabsAdapter(FragmentActivity activity, TabHost tabHost, ViewPager pager) {
-            super(activity.getSupportFragmentManager());
+        public PageFragmentAdapter(FragmentActivity activity, FragmentManager fm) {
+            super(fm);
             mContext = activity;
-            mTabHost = tabHost;
-            mViewPager = pager;
-            mTabHost.setOnTabChangedListener(this);
-            mViewPager.setAdapter(this);
-            mViewPager.setOnPageChangeListener(this);
         }
 
-        public void addTab(TabHost.TabSpec tabSpec, Class<?> clss, Bundle args) {
-            tabSpec.setContent(new DummyTabFactory(mContext));
-            String tag = tabSpec.getTag();
-
-            TabInfo info = new TabInfo(tag, clss, args);
-            mTabs.add(info);
-            mTabHost.addTab(tabSpec);
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getCount() {
-            return mTabs.size();
+        public void addPage(PageInfo page) {
+            mPages.add(page);
         }
 
         @Override
         public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+            PageInfo page = mPages.get(position);
+            return Fragment.instantiate(mContext,
+                    page.getFragmentClass().getName(), page.getArgs());
         }
 
         @Override
-        public void onTabChanged(String tabId) {
-            int position = mTabHost.getCurrentTab();
-            mViewPager.setCurrentItem(position);
+        public int getCount() {
+            return mPages.size();
         }
 
         @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            // Unfortunately when TabHost changes the current tab, it kindly
-            // also takes care of putting focus on it when not in touch mode.
-            // The jerk.
-            // This hack tries to prevent this from pulling focus out of our
-            // ViewPager.
-            TabWidget widget = mTabHost.getTabWidget();
-            int oldFocusability = widget.getDescendantFocusability();
-            widget.setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
-            mTabHost.setCurrentTab(position);
-            widget.setDescendantFocusability(oldFocusability);
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
+        public String getTitle(int position) {
+            return mPages.get(position).getTextResource().toUpperCase();
         }
 
     }
-    
+
+    public class PageInfo {
+        private String mTextResource;
+        private Class<?> mFragmentClass;
+        private Bundle mArgs;
+
+        public PageInfo(String textResource, Class<?> fragmentClass,
+                Bundle args) {
+            mTextResource = textResource;
+            mFragmentClass = fragmentClass;
+            mArgs = args;
+        }
+
+        public String getTextResource() {
+            return mTextResource;
+        }
+
+        public Class<?> getFragmentClass() {
+            return mFragmentClass;
+        }
+
+        public Bundle getArgs() {
+            return mArgs;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //outState.putParcelable("page_adapter", mPageAdapter.saveState());
+    }
+
     @Override
     public boolean onSearchRequested() {
-    	if(mTabHost.getCurrentTabTag().equals("departures"))
-    	{
-    		View editText = findViewById(R.id.sites);
-			editText.requestFocus();
-    		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-    		imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
-    	} else {
-    		mTabHost.setCurrentTabByTag("search");
-    	}	
-    	return true;
+        if (mPager.getCurrentItem() != 0) {
+            mPager.setCurrentItem(0); // Search.
+            return true;
+        }
+        return false;
     }
 }
