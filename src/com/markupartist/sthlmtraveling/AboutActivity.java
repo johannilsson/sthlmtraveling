0c1e0e27ea70eb0d274633115ae050f834119f8b
Johan Nilsson <github@markupartist.com>
2013-01-05 16:54:20 +0100

    Add legal notice for Google Play Services.

--- src/com/markupartist/sthlmtraveling/AboutActivity.java
+++ src/com/markupartist/sthlmtraveling/AboutActivity.java
@@ -1,15 +1,37 @@
 package com.markupartist.sthlmtraveling;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class AboutActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.web);
-        WebView webView = (WebView) findViewById(R.id.web_view); 
-        webView.loadUrl("file:///android_asset/legal.html");
+        setContentView(R.layout.legal);
+        TextView webView = (TextView) findViewById(R.id.legal_notice);
+
+        String legal = "";
+        try {
+            InputStream is = getAssets().open("legal.html");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            legal += new String(buffer);
+        } catch (IOException e) {
+            ;
+        }
+
+        String licenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(this);
+
+        legal += licenseInfo;
+
+        webView.setText(legal);
     }
 }
