a3b332f240e1f0409d3514bf606b75d1b9161909
Johan Nilsson <github@markupartist.com>
2013-12-15 11:26:18 +0100

    Cosmetic fix, tabs to space.

--- src/com/markupartist/sthlmtraveling/TrafficStatusFragment.java
+++ src/com/markupartist/sthlmtraveling/TrafficStatusFragment.java
@@ -38,191 +38,192 @@ import com.markupartist.sthlmtraveling.provider.deviation.DeviationStore.Traffic
 
 public class TrafficStatusFragment extends BaseFragment {
 
-	private static final String TAG = "TrafficStatusActivity";
-	private LinearLayout mProgress;
-	private GetTrafficStatusTask mGetTrafficStatusTask;
+    private static final String TAG = "TrafficStatusActivity";
+    private LinearLayout mProgress;
+    private GetTrafficStatusTask mGetTrafficStatusTask;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		registerEvent("Traffic status");
+        registerEvent("Traffic status");
 
-	}
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.traffic_status_fragment, container, false);
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.traffic_status_fragment, container,
+                false);
+    }
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		Button allDeviations = (Button) getActivity().findViewById(
-				R.id.traffic_status_btn_all_deviations);
-		allDeviations.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getActivity(),
-						DeviationsActivity.class));
-			}
-		});
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Button allDeviations = (Button) getActivity().findViewById(
+                R.id.traffic_status_btn_all_deviations);
+        allDeviations.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),
+                        DeviationsActivity.class));
+            }
+        });
 
-		mProgress = (LinearLayout) getActivity().findViewById(
-				R.id.search_progress);
-		mProgress.setVisibility(View.GONE);
+        mProgress = (LinearLayout) getActivity().findViewById(
+                R.id.search_progress);
+        mProgress.setVisibility(View.GONE);
 
-		mGetTrafficStatusTask = new GetTrafficStatusTask();
-		mGetTrafficStatusTask.execute();
-		super.onActivityCreated(savedInstanceState);
-	}
-	
-	@Override
-	public void onDestroyView() {
-		mGetTrafficStatusTask.cancel(true);
-		super.onDestroyView();
-	}
+        mGetTrafficStatusTask = new GetTrafficStatusTask();
+        mGetTrafficStatusTask.execute();
+        super.onActivityCreated(savedInstanceState);
+    }
 
-	private void inflateViews(TrafficStatus ts) {
-		LinearLayout typeView = (LinearLayout) getActivity().findViewById(
-				R.id.traffic_status_types);
-		if (typeView != null) {
-			typeView.removeAllViews();
+    @Override
+    public void onDestroyView() {
+        mGetTrafficStatusTask.cancel(true);
+        super.onDestroyView();
+    }
 
-			for (TrafficType tt : ts.trafficTypes) {
-				typeView.addView(inflateTrafficType(tt, typeView));
-			}
-		}
-	}
+    private void inflateViews(TrafficStatus ts) {
+        LinearLayout typeView = (LinearLayout) getActivity().findViewById(
+                R.id.traffic_status_types);
+        if (typeView != null) {
+            typeView.removeAllViews();
 
-	private View inflateTrafficType(TrafficType tt, ViewGroup viewGroup) {
-		View v = getActivity().getLayoutInflater().inflate(
-				R.layout.traffic_status_type, viewGroup, false);
+            for (TrafficType tt : ts.trafficTypes) {
+                typeView.addView(inflateTrafficType(tt, typeView));
+            }
+        }
+    }
 
-		TextView transportText = (TextView) v
-				.findViewById(R.id.traffic_status_type_transport);
-		transportText.setText(getStringResourceForTransport(tt.type));
-		ImageView status = (ImageView) v
-				.findViewById(R.id.traffic_status_status);
-		status.setImageResource(getImageResourceForStatus(tt.status));
+    private View inflateTrafficType(TrafficType tt, ViewGroup viewGroup) {
+        View v = getActivity().getLayoutInflater().inflate(
+                R.layout.traffic_status_type, viewGroup, false);
 
-		ImageView transportImage = (ImageView) v
-				.findViewById(R.id.traffic_status_type_transport_image);
-		transportImage.setImageResource(getImageResourceForTransport(tt.type));
+        TextView transportText = (TextView) v
+                .findViewById(R.id.traffic_status_type_transport);
+        transportText.setText(getStringResourceForTransport(tt.type));
+        ImageView status = (ImageView) v
+                .findViewById(R.id.traffic_status_status);
+        status.setImageResource(getImageResourceForStatus(tt.status));
 
-		LinearLayout events = (LinearLayout) v
-				.findViewById(R.id.traffic_status_type_events);
-		events.removeAllViews();
-		for (TrafficEvent te : tt.events) {
-			events.addView(inflateTrafficEvent(te, events));
-		}
+        ImageView transportImage = (ImageView) v
+                .findViewById(R.id.traffic_status_type_transport_image);
+        transportImage.setImageResource(getImageResourceForTransport(tt.type));
 
-		return v;
-	}
+        LinearLayout events = (LinearLayout) v
+                .findViewById(R.id.traffic_status_type_events);
+        events.removeAllViews();
+        for (TrafficEvent te : tt.events) {
+            events.addView(inflateTrafficEvent(te, events));
+        }
 
-	private View inflateTrafficEvent(TrafficEvent te, ViewGroup viewGroup) {
-		View v = getActivity().getLayoutInflater().inflate(
-				R.layout.traffic_status_event, viewGroup, false);
+        return v;
+    }
 
-		TextView message = (TextView) v
-				.findViewById(R.id.traffic_status_event_message);
-		message.setText(te.message);
-		ImageView status = (ImageView) v
-				.findViewById(R.id.traffic_status_event_status);
-		status.setImageResource(getImageResourceForStatus(te.status));
+    private View inflateTrafficEvent(TrafficEvent te, ViewGroup viewGroup) {
+        View v = getActivity().getLayoutInflater().inflate(
+                R.layout.traffic_status_event, viewGroup, false);
 
-		return v;
-	}
+        TextView message = (TextView) v
+                .findViewById(R.id.traffic_status_event_message);
+        message.setText(te.message);
+        ImageView status = (ImageView) v
+                .findViewById(R.id.traffic_status_event_status);
+        status.setImageResource(getImageResourceForStatus(te.status));
 
-	private int getStringResourceForTransport(String transport) {
-		if ("MET".equals(transport)) {
-			return R.string.metros;
-		} else if ("TRN".equals(transport)) {
-			return R.string.trains;
-		} else if ("TRM".equals(transport)) {
-			return R.string.trams;
-		} else if ("TRC".equals(transport)) {
-			return R.string.tram_cars;
+        return v;
+    }
+
+    private int getStringResourceForTransport(String transport) {
+        if ("MET".equals(transport)) {
+            return R.string.metros;
+        } else if ("TRN".equals(transport)) {
+            return R.string.trains;
+        } else if ("TRM".equals(transport)) {
+            return R.string.trams;
+        } else if ("TRC".equals(transport)) {
+            return R.string.tram_cars;
         } else if ("SHP".equals(transport)) {
             return R.string.boats;
-		} else {
-			return R.string.buses;
-		}
-	}
+        } else {
+            return R.string.buses;
+        }
+    }
 
-	private int getImageResourceForTransport(String transport) {
-		if ("MET".equals(transport)) {
-			return R.drawable.transport_metro;
-		} else if ("TRN".equals(transport)) {
-			return R.drawable.transport_train;
-		} else if ("TRM".equals(transport)) {
-			return R.drawable.transport_lokalbana;
-		} else if ("TRC".equals(transport)) {
-			return R.drawable.transport_tram_car;
-		} else if ("SHP".equals(transport)) {
-		    return R.drawable.transport_boat;
-		} else {
-			return R.drawable.transport_bus;
-		}
-	}
+    private int getImageResourceForTransport(String transport) {
+        if ("MET".equals(transport)) {
+            return R.drawable.transport_metro;
+        } else if ("TRN".equals(transport)) {
+            return R.drawable.transport_train;
+        } else if ("TRM".equals(transport)) {
+            return R.drawable.transport_lokalbana;
+        } else if ("TRC".equals(transport)) {
+            return R.drawable.transport_tram_car;
+        } else if ("SHP".equals(transport)) {
+            return R.drawable.transport_boat;
+        } else {
+            return R.drawable.transport_bus;
+        }
+    }
 
-	private int getImageResourceForStatus(int status) {
-		switch (status) {
-		case 1:
-			return R.drawable.traffic_status_good;
-		case 2:
-			return R.drawable.traffic_status_minor;
-		}
-		return R.drawable.traffic_status_major;
-	}
+    private int getImageResourceForStatus(int status) {
+        switch (status) {
+        case 1:
+            return R.drawable.traffic_status_good;
+        case 2:
+            return R.drawable.traffic_status_minor;
+        }
+        return R.drawable.traffic_status_major;
+    }
 
-	/**
-	 * Show progress dialog.
-	 */
-	private void showProgress() {
-		mProgress.setVisibility(View.VISIBLE);
-	}
+    /**
+     * Show progress dialog.
+     */
+    private void showProgress() {
+        mProgress.setVisibility(View.VISIBLE);
+    }
 
-	/**
-	 * Dismiss the progress dialog.
-	 */
-	private void dismissProgress() {
-		mProgress.setVisibility(View.GONE);
-	}
+    /**
+     * Dismiss the progress dialog.
+     */
+    private void dismissProgress() {
+        mProgress.setVisibility(View.GONE);
+    }
 
-	/**
-	 * Background job for getting {@link TrafficStatus}s.
-	 */
-	private class GetTrafficStatusTask extends
-			AsyncTask<Void, Void, TrafficStatus> {
-		private boolean mWasSuccess = true;
+    /**
+     * Background job for getting {@link TrafficStatus}s.
+     */
+    private class GetTrafficStatusTask extends
+            AsyncTask<Void, Void, TrafficStatus> {
+        private boolean mWasSuccess = true;
 
-		@Override
-		public void onPreExecute() {
-			showProgress();
-		}
+        @Override
+        public void onPreExecute() {
+            showProgress();
+        }
 
-		@Override
-		protected TrafficStatus doInBackground(Void... params) {
-			DeviationStore ds = new DeviationStore();
-			try {
-				return ds.getTrafficStatus();
-			} catch (IOException e) {
-				mWasSuccess = false;
-				return null;
-			}
-		}
+        @Override
+        protected TrafficStatus doInBackground(Void... params) {
+            DeviationStore ds = new DeviationStore();
+            try {
+                return ds.getTrafficStatus();
+            } catch (IOException e) {
+                mWasSuccess = false;
+                return null;
+            }
+        }
 
-		@Override
-		protected void onPostExecute(TrafficStatus result) {
-			dismissProgress();
+        @Override
+        protected void onPostExecute(TrafficStatus result) {
+            dismissProgress();
 
-			if (mWasSuccess) {
-				inflateViews(result);
-			} else {
-				Toast.makeText(getActivity(), R.string.network_problem_message,
-						Toast.LENGTH_LONG).show();
-			}
-		}
-	}
+            if (mWasSuccess) {
+                inflateViews(result);
+            } else {
+                Toast.makeText(getActivity(), R.string.network_problem_message,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
