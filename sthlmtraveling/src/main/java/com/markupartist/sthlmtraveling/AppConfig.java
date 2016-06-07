35947853f20413b0c87f41b155fd9447480e92ed
Johan Nilsson <github@markupartist.com>
2016-06-07 16:09:46 +0200

    Add debug ad id

--- sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/AppConfig.java
+++ sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/AppConfig.java
@@ -9,7 +9,7 @@ import com.markupartist.sthlmtraveling.utils.AdProxy;
 public class AppConfig {
     public static final String ANALYTICS_PROPERTY_KEY = BuildConfig.APP_ANALYTICS_PROPERTY_KEY;
     public static final String APP_VERSION = BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")";
-    public static final String WIDESPACE_SID = BuildConfig.APP_WIDESPACE_SID;
+    public static final String WIDESPACE_SID = BuildConfig.DEBUG ? BuildConfig.APP_WIDESPACE_DEBUG_SID : BuildConfig.APP_WIDESPACE_SID;
     public static final String STHLM_TRAVELING_API_KEY = BuildConfig.APP_STHLMTRAVELING_API_KEY;
     public static final String USER_AGENT = "STHLMTraveling-Android/" + BuildConfig.VERSION_NAME;
     public static final String STHLM_TRAVELING_API_ENDPOINT = "http://api.sthlmtraveling.se/";
