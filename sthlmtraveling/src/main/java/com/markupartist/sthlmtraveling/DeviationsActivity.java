71af55d3d187feb4de6bd134f7aef071a104c968
Anton Ehlert <35575737+a-ehlert@users.noreply.github.com>
2018-04-26 15:16:36 +0200

    Feature/search deviations (#6)
    Adding search functionality with text highlighting in Deviations.

--- sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/DeviationsActivity.java
+++ sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/DeviationsActivity.java
@@ -21,10 +21,20 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextWatcher;
 import android.text.format.Time;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.TextAppearanceSpan;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -33,6 +43,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager.BadTokenException;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleAdapter.ViewBinder;
@@ -57,10 +68,14 @@ public class DeviationsActivity extends BaseListActivity {
 
     private GetDeviationsTask mGetDeviationsTask;
     private ArrayList<Deviation> mDeviationsResult;
+    private ArrayList<Deviation> mAllDeviations;
     private LinearLayout mProgress;
+    private TextView mSearchEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
 
 //        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
@@ -73,9 +88,34 @@ public class DeviationsActivity extends BaseListActivity {
         mProgress.setVisibility(View.GONE);
 
         initActionBar();
+        if(savedInstanceState != null){
+            fillData(savedInstanceState.<Deviation>getParcelableArrayList("deviationsResult"));
+        }
+        else{
+            loadDeviations();
+        }
 
-        loadDeviations();
         registerForContextMenu(getListView());
+
+        mSearchEditText = (EditText) this.findViewById(R.id.deviations_search_field);
+        mSearchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //unused
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d(TAG, "onTextChanged");
+                //unused
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filterResult(editable.toString());
+            }
+        });
+        mSearchEditText.clearFocus();
     }
 
     @Override
@@ -102,6 +142,7 @@ public class DeviationsActivity extends BaseListActivity {
             (ArrayList<Deviation>) getLastNonConfigurationInstance();
         if (result != null) {
             fillData(result);
+            mAllDeviations = result;
         } else {
             mGetDeviationsTask = new GetDeviationsTask();
             mGetDeviationsTask.execute();
@@ -165,12 +206,14 @@ public class DeviationsActivity extends BaseListActivity {
             @Override
             public boolean setViewValue(View view, Object data,
                     String textRepresentation) {
+
                 switch (view.getId()) {
-                case R.id.deviation_header:
                 case R.id.deviation_details:
+                case R.id.deviation_header:
                 case R.id.deviation_created:
                 case R.id.deviation_scope:
-                    ((TextView)view).setText(textRepresentation);
+                    String[] word = mSearchEditText.getText().toString().split(" ");
+                    ((TextView)view).setText(highlight(textRepresentation, word));
                     return true;
                 }
                 return false;
@@ -196,7 +239,7 @@ public class DeviationsActivity extends BaseListActivity {
         AdapterView.AdapterContextMenuInfo menuInfo =
             (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         Deviation deviation = mDeviationsResult.get(menuInfo.position);
-        share(deviation.getHeader(), deviation.getDetails());
+        share(deviation.getHeader(), deviation.getDetails().toString());
         return true;
     }
 
@@ -207,6 +250,7 @@ public class DeviationsActivity extends BaseListActivity {
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("deviationsResult", mDeviationsResult);
         super.onSaveInstanceState(outState);
         saveGetDeviationsTask(outState);
     }
@@ -294,6 +338,7 @@ public class DeviationsActivity extends BaseListActivity {
 
     @Override
     protected void onDestroy() {
+        mSearchEditText.setText("");
         super.onDestroy();
 
         onCancelGetDeviationsTask();
@@ -337,6 +382,7 @@ public class DeviationsActivity extends BaseListActivity {
 
             if (mWasSuccess) {
                 fillData(result);
+                mAllDeviations = result;
             } else {
                 try {
                     showDialog(DIALOG_GET_DEVIATIONS_NETWORK_PROBLEM);
@@ -361,4 +407,53 @@ public class DeviationsActivity extends BaseListActivity {
 
         startActivity(Intent.createChooser(intent, getText(R.string.share_label)));
     }
+
+    // Pretty ugly solution fix it NOW
+    private void filterResult(String search){
+        if (mAllDeviations == null) return;
+        String[] words = search.toLowerCase().split(" ");
+        ArrayList<Deviation> filteredResult = new ArrayList<Deviation>();
+        boolean match = false;
+        for(Deviation deviation:mAllDeviations){
+            for(String word:words){
+                boolean checkA = deviation.getHeader().toLowerCase().contains(word);
+                boolean checkB = deviation.getDetails().toString().toLowerCase().contains(word);
+                boolean checkC = deviation.getScope().toLowerCase().contains(word);
+                if(!(checkA||checkB||checkC)){
+                    match = false;
+                    break;
+                }
+                else {
+                    match = true;
+                }
+            }
+            if (match) {
+                filteredResult.add(deviation);
+            }
+        }
+        fillData(filteredResult);
+    }
+
+    /***
+     * Writed by Team 8, highlight all words contained within a string
+     */
+    private Spannable highlight(String str, String[] words){
+        Spannable spn = new SpannableString(str);
+        for(String word:words)
+        if(word.length() > 1) {
+            str = str.toLowerCase();
+            word = word.toLowerCase();
+            int index_start = str.indexOf(word);
+            while (index_start > -1) {
+
+                BackgroundColorSpan hl = new BackgroundColorSpan(Color.YELLOW);//new TextAppearanceSpan(null, Typeface.BOLD, -1, highlightColor, null);
+
+                spn.setSpan(hl, index_start, index_start + word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                index_start = str.indexOf(word, index_start + word.length());
+            }
+        }
+        return spn;
+    }
+
+
 }
