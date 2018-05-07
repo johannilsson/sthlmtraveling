8216c1a42c03c78e6ea96697f6fad56debaac13a
FilipAppelgren <37618845+FilipAppelgren@users.noreply.github.com>
2018-05-07 10:55:01 +0200

    Update MyApplication.java

--- sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/MyApplication.java
+++ sthlmtraveling/src/main/java/com/markupartist/sthlmtraveling/MyApplication.java
@@ -11,7 +11,6 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Logger;
 import com.google.android.gms.analytics.Tracker;
@@ -37,8 +36,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //Fabric.with(this, new Crashlytics());
-        Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build());
+        Fabric.with(this, new Crashlytics());
         reloadLocaleForApplication();
 
         DataMigrationService.startService(this);
